a91f7c189c6b135b334a79aa29639bfaf91698b4
Ryan Bartley <ryanbartley@gmail.com>
2016-11-02 16:12:53 -0400

    Updated cinder_path lookup

--- proj/cmake/Cinder-RapidJsonConfig.cmake
+++ proj/cmake/Cinder-RapidJsonConfig.cmake
@@ -1,7 +1,7 @@
 if( NOT TARGET Cinder-RapidJson )
 	
   get_filename_component( BLOCKS_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE )
-  get_filename_component( CINDER_PATH "${BLOCKS_PATH}/../../include" ABSOLUTE )
+  get_filename_component( CINDER_RAPIDJSON_PATH "${BLOCKS_PATH}/../../include" ABSOLUTE )
   get_filename_component( RAPIDJSON_INCLUDE_PATH "${BLOCKS_PATH}/rapidjson/include" ABSOLUTE )
 
 	# Add OSC block as a dependency
